977f15be18fbad45313b633e15d23f76dc841dab
Dylan <67800304+dylanrmp@users.noreply.github.com>
2025-01-24 07:04:43 +0100

    Add documentation

--- js/flights.js
+++ js/flights.js
@@ -33,21 +33,25 @@ flightEntries = [
     { time: "18:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
   ],
 
-];
+]; // usually you'd fetch this from a web api
 
-currentPage = 0;
+currentPage = 0; // usually you'd fetch this from a web api aswell
 
 updateTable = (pageNumber) => {
   const table = document.getElementById("flights");
 
   const page = flightEntries[pageNumber];
 
+  // Clear the current table
   const rows = table.rows.length;
   for (let i = rows - 1; i > 0; i--) {
     table.deleteRow(i);
   }
 
+  // Populate the table
   for (let i = 0; i < page.length; i++) {
+
+    // Insert the rows & cells
     const row = table.insertRow(i + 1);
     const time = row.insertCell(0);
     const destination = row.insertCell(1);
@@ -55,12 +59,14 @@ updateTable = (pageNumber) => {
     const airline = row.insertCell(3);
     const status = row.insertCell(4);
 
+    // Insert the fetched data
     time.innerText = page[i].time;
     destination.innerText = page[i].destination;
     flight.innerText = page[i].flight;
     airline.innerText = page[i].airline;
     status.innerText = page[i].status;
 
+    // Adjust the color on the status type
     switch(page[i].status) {
       case "vertrokken":
         status.style.color = "gray";
@@ -86,26 +92,35 @@ updateTable = (pageNumber) => {
   }
 }
 
+// Renders an amount of pagination indicators based on the page count provided by the api.
 renderPageButtons = () => {
   for (let i = 0; i < flightEntries.length; i++) {
+    // render the indicator
     document.getElementById("pageNumbers").innerHTML += `<div id=\"pageNumber ${i}\">${i+1}</div>`;
 
     if (i === 0) {
+      // we want to highlight the first indicator, because we start at the first page.
       document.getElementById(`pageNumber ${i}`).style.background = "yellow";
     }
   }
 }
 
+// Switches to the next page
+// Updates the page indicator color
+// Updates the table content
 nextPage = () => {
   const previousPage = currentPage;
 
   if (currentPage !== flightEntries.length - 1) {
+    // if current page is not the last page, continue
     currentPage += 1;
   }
   else {
+    // start over
     currentPage = 0;
   }
 
+  // paints the indicator of the current page yellow, previous page back to default
   document.getElementById(`pageNumber ${previousPage}`).style.background = "#ded6cb";
   document.getElementById(`pageNumber ${currentPage}`).style.background = "yellow";
 
--- js/progressBar.js
+++ js/progressBar.js
@@ -1,12 +1,18 @@
 startProgressBar = (duration, pauseTime) => {
   const indicator = document.getElementById("indicator");
+
+  // Enable the transition / fill animation
   indicator.style.transition = "width 5s linear";
   indicator.style.width = "100%";
 
+  // Call this function after the provided duration.
   setTimeout(() => {
+
+    // makes sure that the transition won't play backwards & indicator resets instantly.
     indicator.style.transition = "none";
     indicator.style.width = "0%";
 
+    // Restart the progress bar after a short pause, without a pause the progress bar will break.
     setTimeout( () => {
       startProgressBar(duration, pauseTime);
     }, pauseTime);
