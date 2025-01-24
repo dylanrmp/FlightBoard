611873a4409f2f34f2dad3a2cf7ee8a0b207d8a2
Dylan <67800304+dylanrmp@users.noreply.github.com>
2025-01-24 06:32:05 +0100

    Implement variable colors

--- js/flights.js
+++ js/flights.js
@@ -1,36 +1,36 @@
 flightEntries = [
   [
     { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
+    { time: "18:00", destination: "paramaribo (pbm)", flight: "py 993", airline: "surinam airways", status: "gate geopend"},
+    { time: "18:10", destination: "london luton (ltn)", flight: "ezy 2160", airline: "easyjet", status: "gate sluit"},
+    { time: "19:00", destination: "cairo (cai)", flight: "ms 758", airline: "egypt air", status: "vertrokken"},
+    { time: "19:22", destination: "kos (kgs)", flight: "or 265", airline: "tui fly", status: "gate veranderd"},
+    { time: "19:39", destination: "hurghada (hrg)", flight: "cnd 217", airline: "corendon dutch airlines", status: "gate geopend"},
+    { time: "19:50", destination: "lyon (lys)", flight: "af 8423", airline: "air france", status: "gate geopend"},
+    { time: "20:00", destination: "dublin (dub)", flight: "su 3406", airline: "aeroflot", status: "op schema"},
+    { time: "20:10", destination: "belfast city airport (bhd)", flight: "su 3406", airline: "air france", status: "boarden gestart"},
+    { time: "20:18", destination: "birmingham (bhx)", flight: "af 8423", airline: "flybe", status: "op schema"},
   ],
 
   [
     { time: "17:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
+    { time: "17:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
+    { time: "17:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "gate geopend"},
+    { time: "17:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "gate geopend"},
+    { time: "17:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
+    { time: "17:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
+    { time: "17:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
+    { time: "17:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "gate sluit"},
+    { time: "17:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
+    { time: "17:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
   ],
 
   [
     { time: "18:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
-    { time: "16:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
+    { time: "18:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "gate sluit"},
+    { time: "18:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "boarden gestart"},
+    { time: "18:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
+    { time: "18:20", destination: "dubai (dxb)", flight: "af 8423", airline: "air france", status: "vertrokken"},
   ],
 
 ];
@@ -60,6 +60,29 @@ updateTable = (pageNumber) => {
     flight.innerText = page[i].flight;
     airline.innerText = page[i].airline;
     status.innerText = page[i].status;
+
+    switch(page[i].status) {
+      case "vertrokken":
+        status.style.color = "gray";
+        break;
+      case "gate geopend":
+        status.style.color = "green";
+        break;
+      case "gate sluit":
+        status.style.color = "red";
+        break;
+      case "op schema":
+        status.style.color = "blue";
+        break;
+      case "boarden gestart":
+        status.style.color = "#ff4d01";
+        break;
+      case "gate veranderd":
+        status.style.color = "#ff4d01";
+        break;
+      default:
+        status.style.color = "black";
+    }
   }
 }
 
